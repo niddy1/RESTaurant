d8aa5dbfc45917f1d15ef9f0cbb54c182a777073
Ian Andreu <ianandreu87@gmail.com>
2015-08-30 17:13:04 -0400

    tables retouched, party edit styled, kitchen retouched

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -8,6 +8,9 @@ function kitchenTimer() {
     else if ( (parseFloat(times[i].innerHTML)) > 300){
       times[i].closest(".kitchen-orders").classList.add("kitchen-yellow");
     }
+    else if ( (parseFloat(times[i].innerHTML)) > 0){
+      times[i].closest(".kitchen-orders").classList.add("kitchen-green");
+    }
   }
 }
 
